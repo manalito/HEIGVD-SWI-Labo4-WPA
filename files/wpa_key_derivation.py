4b360f49757f370583b8d630b71656478e74fb97
manalito <aurelien.siu@heigvd.ch>
2020-04-25 23:44:41 +0200

    first step script in progress, require data retrieve to be complete

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -18,11 +18,12 @@ __status__ 		= "Prototype"
 
 from scapy.all import *
 from binascii import a2b_hex, b2a_hex
-from pbkdf2_math import pbkdf2_hex
 from pbkdf2 import *
+from pbkdf2_math import pbkdf2_hex
 from numpy import array_split
 from numpy import array
 import hmac, hashlib
+import codecs
 
 def customPRF512(key,A,B):
     """
@@ -43,13 +44,20 @@ wpa=rdpcap("wpa_handshake.cap")
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = "SWI"
-APmac       = a2b_hex("cebcc8fdcab7")
-Clientmac   = a2b_hex("0013efd015bd")
+
+# Récupération des valeurs pouvant être trouvée dans la capture
+# le ssid peut être récupéré dans le premier paquet de la capture
+ssid = wpa[0].info.decode()
+# l'adresse mac de l'AP se trouve également dans le premier paquet car c'est l'AP qui l'envoie en broadcast
+APmac = a2b_hex(wpa[0].addr2.replace(":", ""))
+# comme l'AP envoie le message en broadcast dans le premier paquet, l'adresse du client est trouvable seulement dès le deuxième paquet
+Clientmac = a2b_hex(wpa[1].addr1.replace(":", ""))
 
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+# Le paquet 6 (wpa[5]) de la capture contient le nonce de l'AP
+ANonce = a2b_hex(wpa[5].load.hex()[26:90])
+# Le paquet 7 (wpa[6]) de la capture contient le nonce du client
+SNonce = a2b_hex(wpa[6].load.hex()[26:90])
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
